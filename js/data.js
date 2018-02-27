a3674c9fe274f7b81fc857157065281d40e93bed
EduardBrazhnikov <edikbrazhnikov@gmail.com>
2018-02-27 17:41:35 +0200

    new
    new

---
+++ js/data.js
@@ -0,0 +1,108 @@
+"use strict"
+var str = [
+	{
+		"name": "Ежик",
+		"dicription": "Описание ежика",
+		"price": 100,
+		"img": "https://static.detmir.st/medias/sys_master/images/images/hcc/hbd/9778090147870.jpg",
+		"Availability": 1
+	},
+	{
+		"name": "Мишка",
+		"dicription": "Описание мишки",
+		"price": 200,
+		"img": "http://www.ikea.com/ru/ru/images/products/brunb-ern-magkaa-igruska__0531366_PE647283_S4.JPG",
+		"Availability": 1
+	},
+	{
+		"name": "Кошка",
+		"dicription": "Описание кошки",
+		"price": 300,
+		"img": "http://planettoys.ua/wp-content/uploads/TY-Secret-Life-of-Pets-962981.jpg",
+		"Availability": 0
+	},
+	{
+		"name": "Собака",
+		"dicription": "Описание собаки",
+		"price": 300,
+		"img": "https://i2.rozetka.ua/goods/2268924/orange_4751025413929_images_2268924644.jpg",
+		"Availability": 1
+	},
+	{
+		"name": "Чебурашка",
+		"dicription": "Описание чебурашки",
+		"price": 400,
+		"img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbD0KDfTrntpfMBGypA_Q6JHwPDT3um8ofeSIgEMnaD_VoWjU7",
+		"Availability": 0
+	},
+	{
+		"name": "Утка",
+		"dicription": "Описание утки",
+		"price": 500,
+		"img": "http://tigres.com.ua/-/uploads/ru/00/00/03/77/big-1656989534.jpg",
+		"Availability": 1
+	},
+	{
+		"name": "Лиса",
+		"dicription": "Описание лисы",
+		"price": 600,
+		"img": "https://variety-store.ru/static/productimage/name/18197-beanie-boos-lisenok-slick-25-sm-8bafae.jpg",
+		"Availability": 1
+	},
+	{
+		"name": "Осел",
+		"dicription": "Описание осла",
+		"price": 700,
+		"img": "http://papus666.narod.ru/clipart/o/osel/osel22.png", 
+		"Availability": 1
+	},
+	{
+		"name": "Слон",
+		"dicription": "Описание слона",
+		"price": 800,
+		"img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw_-eeHWG23A9qPIQbT3_mkF1jDH0V-LwxWmQMlDnVYZlTUOw1Ag",
+		"Availability": 0
+	},
+	{
+		"name": "Кролик",
+		"dicription": "Описание кролика",
+		"price": 900,
+		"img": "http://mirdetstva.ru/sites/default/files/items/33207.jpg",
+		"Availability": 1
+	},
+	{
+		"name": "Пингвин",
+		"dicription": "Описание пингвина",
+		"price": 1000,
+		"img": "https://variety-store.ru/static/productimage/name/18523-myagkaya-igrushka-ty-36008-beanie-boos-pingvin-waddles-15-sm-6b22a0.jpg",
+		"Availability": 1
+	},
+	{
+		"name": "Овечка",
+		"dicription": "Описание овечки",
+		"price": 1100,
+		"img": "https://ae01.alicdn.com/kf/HTB1nwJeHpXXXXXMXVXXq6xXFXXXe.jpg",
+		"Availability": 1
+	},
+	{
+		"name": "Олень",
+		"dicription": "Описание оленя",
+		"price": 1200,
+		"img": "https://lanita.ru/images/offer/11227/8/0761898/0761898.jpg",
+		"Availability": 1
+	},
+	{
+		"name": "Лягушка",
+		"dicription": "Описание лягушки",
+		"price": 1300,
+		"img": "https://ozon-st.cdn.ngenix.net/multimedia/c300/1012357589.jpg",
+		"Availability": 1
+	},
+	{
+		"name": "Панда",
+		"dicription": "Описание панды",
+		"price": 1400,
+		"img": "https://www.lekea.pl/sklep/4569-large_default/kramig-pluszak-bialy-czarny-panda-pluszak.jpg",
+		"Availability": 1
+	}
+]
